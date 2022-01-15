e4f6a4d73dfc1dc389eeeb7ce03eeda4ea262f6b
William W <81191122+websterwh16@users.noreply.github.com>
2022-01-15 13:34:04 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from ntpath import join
 import os
 from pypresence import Presence
+import psutil
 import time
 from os import system
 
 os.system('cls')
-client_id = input(str('Client ID> '))
+client_id = '931971236366544976'
 RPC = Presence(client_id)
 RPC.connect() 
 
@@ -14,5 +16,5 @@ while True:
     cpu_per = round(psutil.cpu_percent(),1)
     mem = psutil.virtual_memory()
     mem_per = round(psutil.virtual_memory().percent,1)
-    RPC.update(details="RAM: "+str(mem_per)+"%", state="CPU: "+str(cpu_per)+"%", large_image='pc')
+    RPC.update(details="RAM: "+str(mem_per)+"%", state="CPU: "+str(cpu_per)+"%", large_image='pc', large_text='ur gay', buttons=[{"label": "Github Repo", "url": "https://github.com/websterwh16/simple-system-info-rpc"}])
     time.sleep(15)
