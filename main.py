d4b0986853a009e5450f972038b243bb2f0cff59
websterwh16 <websterwh16@gmail.com>
2022-01-15 10:22:50 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from pypresence import Presence
+import psutil
+import time
+from os import system
+
+os.system('cls')
+client_id = input(str('Client ID> '))
+RPC = Presence(client_id)
+RPC.connect() 
+
+os.system('title System Info RPC')
+print('Running RPC')
+while True: 
+    cpu_per = round(psutil.cpu_percent(),1)
+    mem = psutil.virtual_memory()
+    mem_per = round(psutil.virtual_memory().percent,1)
+    RPC.update(details="RAM: "+str(mem_per)+"%", state="CPU: "+str(cpu_per)+"%", large_image='pc')
+    time.sleep(15)
